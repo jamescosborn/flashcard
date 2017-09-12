7713214f6196c9aba45cda1b655872c2c7dd0b65
Elliot Burin and James Osborn <james@>
2017-09-12 15:28:58 -0700

    add index, descriptions

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#description-showing").toggle();
+  });
+
+  $(".clickable2").click(function() {
+    $("#description-showing2").toggle();
+  });
+
+  $(".clickable3").click(function() {
+    $("#description-showing3").toggle();
+  });
+
+  $(".clickable4").click(function() {
+    $("#description-showing4").toggle();
+
+  });
+
+  $(".clickable5").click(function() {
+    $("#description-showing5").toggle();
+  });
+});
